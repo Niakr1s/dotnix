8e859d62df5d6dc8febed65aba187d82b8393f54
Niakr1s <pavel2188@gmail.com>
2026-03-03 01:45:59 +0300

    removed zsh, try to use pure bash from now on

--- home.nix
+++ home.nix
@@ -15,20 +15,6 @@
     };
   };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      la = "ls -la";
-      update = "sudo nixos-rebuild switch --flake /home/nea/.dotnix#desktop";
-    };
-    history.size = 10000;
-  };
-
   programs.git = {
     enable = true;
     settings = {
