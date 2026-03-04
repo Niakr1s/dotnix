914fd0936284ad1cc8049fb069641979abaf47ca
Niakr1s <pavel2188@gmail.com>
2026-03-04 18:41:56 +0300

    moved common config into common.nix, hope didn't miss anything

---
+++ hosts/common/common.nix
@@ -0,0 +1,277 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  hostname,
+  unstablePkgs,
+  username,
+  stateVersion,
+  ...
+}: {
+  # Latest kernel by default
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Garbage collection daily
+  nix.gc = {
+    automatic = true;
+    dates = "21:00";
+  };
+
+  networking.hostName = "${hostname}"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Moscow";
+
+  # These variables will be set by PAM early in the login process.
+  environment.sessionVariables = {
+    # To enable one-to-one trackpad scrolling in Firefox
+    MOZ_USE_XINPUT2 = "1";
+  };
+
+  # These variables will be set on shell initialisation (e.g. in /etc/profile).
+  environment.variables = {
+    EDITOR = "vim";
+    VISUAL = "vim";
+  };
+
+  services.v2raya = {
+    enable = true;
+    package = pkgs.unstable.v2raya;
+    cliPackage = pkgs.unstable.xray;
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    torrentingPort = 6881;
+    webuiPort = 8080;
+    serverConfig = {
+      BitTorrent = {
+        Session = {
+          DefaultSavePath = "/srv/torrents"; # systemd will create this directory for us
+          TempPath = "/srv/torrents/temp";
+          TorrentExportDirectory = "/srv/torrents/.torrents";
+          AlternativeGlobalDLSpeedLimit = 6000;
+          DisableAutoTMMByDefault = false;
+          GlobalMaxRatio = 1; # stop seeding after ratio 1
+          ShareLimitAction = "Stop";
+          QueueingSystemEnabled = false;
+          SubcategoriesEnabled = true;
+        };
+      };
+      Core = {
+        AutoDeleteAddedTorrentFile = "IfAdded";
+      };
+      Preferences = {
+        WebUI = {
+          Username = "${username}";
+          Password_PBKDF2 = "@ByteArray(agI2Tr50yXx8i5Gm9kTfkA==:79jfEujByGcX3FQTbLt2IIm4t7pSxfQhwVIcVFOlTKLtJ1XIJPnDN28+w2udq2ksKpr3UUjxKoCYO6WzaiT+8w==)";
+        };
+      };
+    };
+  };
+
+  # man tmpfiles.d
+  systemd.tmpfiles.rules = [
+    "R /var/lib/qBittorrent/qBittorrent/config/categories.json - - - - -"
+    "C /var/lib/qBittorrent/qBittorrent/config/categories.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/categories.json"
+    "R /var/lib/qBittorrent/qBittorrent/config/watched_folders.json - - - - -"
+    "C /var/lib/qBittorrent/qBittorrent/config/watched_folders.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/watched_folders.json"
+    "d /srv/torrents 2770 qbittorrent qbittorrent - -"
+  ];
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_DK.UTF-8";
+
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  services.gnome.core-apps.enable = true;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-clocks
+    #gnome-control-center
+    gnome-initial-setup
+    gnome-music
+    #pkgs.gnome-connections
+    pkgs.gnome-contacts
+    pkgs.gnome-tour
+    #pkgs.snapshot
+    cheese
+    epiphany
+    evince
+    geary
+    totem
+    yelp
+    decibels
+    snapshot
+    showtime
+  ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us,ru";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "video" "audio" "disk" "networkmanager" "qbittorrent"];
+    uid = 1000;
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+    ];
+  };
+
+  environment.shellAliases = {
+    ll = "ls -l";
+    la = "ls -la";
+  };
+
+  environment.shells = with pkgs; [zsh];
+
+  system.userActivationScripts.zshrc = "touch .zshrc";
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    syntaxHighlighting = {
+      enable = true;
+    };
+
+    autosuggestions = {
+      enable = true;
+      async = true;
+    };
+
+    ohMyZsh = {
+      enable = true;
+      plugins = [
+        "git"
+      ];
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.firefox = {
+    enable = true;
+
+    languagePacks = ["en-US"];
+
+    preferences = {
+      "privacy.resistFingerprinting" = true;
+    };
+
+    policies = {
+      DisableTelemetry = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+    btop-cuda
+    tree
+    bat
+    yazi
+    fzf
+    ripgrep
+
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.just-perfection
+    gnomeExtensions.forge
+    dconf-editor
+
+    protonup-ng # GUI for installing custom Proton versions like GE_Proton
+    mangohud
+    lutris
+    retroarch
+
+    # ------
+
+    # On x86_64-linux, the wine package supports by default both 32- and 64-bit applications.
+    # On every other platform, the wine package supports by default only 32-bit applications.
+
+    # wineWow64Packages.stable # support both 32- and 64-bit applications
+    # wine # support 32-bit only (read above!)
+    # (wine.override { wineBuild = "wine64"; }) # support 64-bit only
+    # wine64 # support 64-bit only
+    # wineWow64Packages.staging # wine-staging (version with experimental features)
+    # winetricks # winetricks (all versions)
+    wineWow64Packages.waylandFull # native wayland support (unstable)
+
+    # ------
+  ];
+
+  fonts.enableDefaultPackages = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "${stateVersion}"; # Set this to first installed version, and then don't change it
+}
--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -16,15 +16,16 @@
 in {
   imports = [
     ./hardware-configuration.nix
+    ../common/common.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPackages = pkgs.linuxPackages_6_18; # Nvidia compatibility
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_18; # Nvidia compatibility
 
+  # Encryption
   boot.initrd.luks.devices = {
     root = {
       device = "/dev/nvme0n1p2";
@@ -32,134 +33,10 @@ in {
     };
   };
 
-  # Garbage collection daily
-  nix.gc = {
-    automatic = true;
-    dates = "21:00";
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  networking.hostName = "${hostname}"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  time.timeZone = "Europe/Moscow";
-
-  # These variables will be set by PAM early in the login process.
-  environment.sessionVariables = {
-    MOZ_USE_XINPUT2 = "1";
-  };
-
-  # These variables will be set on shell initialisation (e.g. in /etc/profile).
-  environment.variables = {
-    EDITOR = "vim";
-    VISUAL = "vim";
-  };
-
-  services.v2raya = {
-    enable = true;
-    package = pkgs.unstable.v2raya;
-    cliPackage = pkgs.unstable.xray;
-  };
-
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
     loadModels = ["gemma3:12b"];
-  };
-
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;
-    torrentingPort = 6881;
-    webuiPort = 8080;
-    serverConfig = {
-      BitTorrent = {
-        Session = {
-          DefaultSavePath = "/srv/torrents"; # systemd will create this directory for us
-          TempPath = "/srv/torrents/temp";
-          TorrentExportDirectory = "/srv/torrents/.torrents";
-          AlternativeGlobalDLSpeedLimit = 6000;
-          DisableAutoTMMByDefault = false;
-          GlobalMaxRatio = 1; # stop seeding after ratio 1
-          ShareLimitAction = "Stop";
-          QueueingSystemEnabled = false;
-          SubcategoriesEnabled = true;
-        };
-      };
-      Core = {
-        AutoDeleteAddedTorrentFile = "IfAdded";
-      };
-      Preferences = {
-        WebUI = {
-          Username = "${username}";
-          Password_PBKDF2 = "@ByteArray(agI2Tr50yXx8i5Gm9kTfkA==:79jfEujByGcX3FQTbLt2IIm4t7pSxfQhwVIcVFOlTKLtJ1XIJPnDN28+w2udq2ksKpr3UUjxKoCYO6WzaiT+8w==)";
-        };
-      };
-    };
-  };
-
-  # man tmpfiles.d
-  systemd.tmpfiles.rules = [
-    "R /var/lib/qBittorrent/qBittorrent/config/categories.json - - - - -"
-    "C /var/lib/qBittorrent/qBittorrent/config/categories.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/categories.json"
-    "R /var/lib/qBittorrent/qBittorrent/config/watched_folders.json - - - - -"
-    "C /var/lib/qBittorrent/qBittorrent/config/watched_folders.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/watched_folders.json"
-    "d /srv/torrents 2770 qbittorrent qbittorrent - -"
-  ];
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_DK.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  services.gnome.core-apps.enable = true;
-  services.gnome.core-developer-tools.enable = false;
-  services.gnome.games.enable = true;
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-clocks
-    #gnome-control-center
-    gnome-initial-setup
-    gnome-music
-    #pkgs.gnome-connections
-    pkgs.gnome-contacts
-    pkgs.gnome-tour
-    #pkgs.snapshot
-    cheese
-    epiphany
-    evince
-    geary
-    totem
-    yelp
-    decibels
-    snapshot
-    showtime
-  ];
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
-      xdg-desktop-portal-gtk
-    ];
-  };
-
-  # Hyprland
-  programs.hyprland = {
-    enable = true;
-    # set the flake package
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   # https://github.com/lutris/docs/blob/master/HowToEsync.md
@@ -230,166 +107,8 @@ in {
     nvidiaSettings = true;
   };
 
-  # enable the x11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us,ru";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    extraGroups = ["wheel" "video" "audio" "disk" "networkmanager" "qbittorrent"];
-    uid = 1000;
-    shell = pkgs.zsh;
-    packages = with pkgs; [
-    ];
-  };
-
-  environment.shellAliases = {
-    ll = "ls -l";
-    la = "ls -la";
-  };
-
-  environment.shells = with pkgs; [zsh];
-
-  system.userActivationScripts.zshrc = "touch .zshrc";
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-
-    syntaxHighlighting = {
-      enable = true;
-    };
-
-    autosuggestions = {
-      enable = true;
-      async = true;
-    };
-
-    ohMyZsh = {
-      enable = true;
-      plugins = [
-        "git"
-      ];
-      theme = "robbyrussell";
-    };
-  };
-
-  programs.firefox = {
-    enable = true;
-
-    languagePacks = ["en-US"];
-
-    preferences = {
-      "privacy.resistFingerprinting" = true;
-    };
-
-    policies = {
-      DisableTelemetry = true;
-    };
-  };
+  # services.libinput.enable = true; TODO: enable in in laptop
 
   programs.steam.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    curl
-    git
-    btop-cuda
-    tree
-    bat
-    yazi
-    fzf
-    ripgrep
-
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.just-perfection
-    gnomeExtensions.forge
-    dconf-editor
-
-    protonup-ng # GUI for installing custom Proton versions like GE_Proton
-    mangohud
-    lutris
-    retroarch
-
-    # ------
-
-    # On x86_64-linux, the wine package supports by default both 32- and 64-bit applications.
-    # On every other platform, the wine package supports by default only 32-bit applications.
-
-    # wineWow64Packages.stable # support both 32- and 64-bit applications
-    # wine # support 32-bit only (read above!)
-    # (wine.override { wineBuild = "wine64"; }) # support 64-bit only
-    # wine64 # support 64-bit only
-    # wineWow64Packages.staging # wine-staging (version with experimental features)
-    # winetricks # winetricks (all versions)
-    wineWow64Packages.waylandFull # native wayland support (unstable)
-
-    # ------
-  ];
-
-  fonts.enableDefaultPackages = true;
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-color-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-  ];
-
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  system.stateVersion = "${stateVersion}"; # Set this to first installed version, and then don't change it
 }
