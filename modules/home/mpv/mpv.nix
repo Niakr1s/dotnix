f750e17d416a8bdc428d829295593a95a174f1fa
Niakr1s <pavel2188@gmail.com>
2026-03-06 02:11:35 +0300

    moved mpv to a module

---
+++ modules/home/mpv/mpv.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  nixpkgs-unstable,
+  stateVersion,
+  hostname,
+  username,
+  ...
+}: let
+in {
+  xdg.configFile."mpv" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/home/${username}/.dotnix/config/mpv";
+    recursive = true;
+  };
+
+  programs.mpv = {
+    enable = true;
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+          ffmpeg = pkgs.ffmpeg-full;
+        };
+      }
+    );
+  };
+}
