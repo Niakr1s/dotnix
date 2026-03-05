22cde5b58dae7bb8ddefe4c8080f6a793def941a
Niakr1s <pavel2188@gmail.com>
2026-03-06 00:09:22 +0300

    mpv changed to handle uosc by itself

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -105,9 +105,27 @@ in {
     };
   };
 
-  programs.mpv.enable = true;
-  xdg.configFile."mpv" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/${username}/.dotnix/config/mpv";
-    recursive = true;
+  programs.mpv = {
+    enable = true;
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          sponsorblock
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+          ffmpeg = pkgs.ffmpeg-full;
+        };
+      }
+    );
+
+    config = {
+      profile = "high-quality";
+      ytdl-format = "bestvideo+bestaudio";
+      cache-default = 4000000;
+    };
   };
 }
