02516e2582b6c7ab605d16c996dcef18ff2f3086
Niakr1s <pavel2188@gmail.com>
2026-03-05 09:45:04 +0300

    moved hyprland out

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -60,15 +60,6 @@
     ];
   };
 
-  # Hyprland
-  programs.hyprland = {
-    enable = true;
-    # set the flake package
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-  };
-
   # Configure keymap in X11
   services.xserver.xkb.layout = "us,ru";
 
