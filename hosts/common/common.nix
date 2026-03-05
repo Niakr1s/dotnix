69e6312b7e3005028b6527dde0acb3db21040de1
Niakr1s <pavel2188@gmail.com>
2026-03-05 09:42:53 +0300

    moved qbittorrent out

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -48,46 +48,6 @@
     cliPackage = pkgs.unstable.xray;
   };
 
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;
-    torrentingPort = 6881;
-    webuiPort = 8080;
-    serverConfig = {
-      BitTorrent = {
-        Session = {
-          DefaultSavePath = "/srv/torrents"; # systemd will create this directory for us
-          TempPath = "/srv/torrents/temp";
-          TorrentExportDirectory = "/srv/torrents/.torrents";
-          AlternativeGlobalDLSpeedLimit = 6000;
-          DisableAutoTMMByDefault = false;
-          GlobalMaxRatio = 1; # stop seeding after ratio 1
-          ShareLimitAction = "Stop";
-          QueueingSystemEnabled = false;
-          SubcategoriesEnabled = true;
-        };
-      };
-      Core = {
-        AutoDeleteAddedTorrentFile = "IfAdded";
-      };
-      Preferences = {
-        WebUI = {
-          Username = "${username}";
-          Password_PBKDF2 = "@ByteArray(agI2Tr50yXx8i5Gm9kTfkA==:79jfEujByGcX3FQTbLt2IIm4t7pSxfQhwVIcVFOlTKLtJ1XIJPnDN28+w2udq2ksKpr3UUjxKoCYO6WzaiT+8w==)";
-        };
-      };
-    };
-  };
-
-  # man tmpfiles.d
-  systemd.tmpfiles.rules = [
-    "R /var/lib/qBittorrent/qBittorrent/config/categories.json - - - - -"
-    "C /var/lib/qBittorrent/qBittorrent/config/categories.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/categories.json"
-    "R /var/lib/qBittorrent/qBittorrent/config/watched_folders.json - - - - -"
-    "C /var/lib/qBittorrent/qBittorrent/config/watched_folders.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/watched_folders.json"
-    "d /srv/torrents 2770 qbittorrent qbittorrent - -"
-  ];
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_DK.UTF-8";
 
@@ -126,7 +86,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
     isNormalUser = true;
-    extraGroups = ["wheel" "video" "audio" "disk" "networkmanager" "qbittorrent"];
+    extraGroups = ["wheel" "video" "audio" "disk" "networkmanager"];
     uid = 1000;
     shell = pkgs.zsh;
     packages = with pkgs; [
---
+++ modules/services/qbittorrent/qbittorrent.nix
@@ -0,0 +1,53 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  hostname,
+  unstablePkgs,
+  username,
+  stateVersion,
+  ...
+}: {
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    torrentingPort = 6881;
+    webuiPort = 8080;
+    serverConfig = {
+      BitTorrent = {
+        Session = {
+          DefaultSavePath = "/srv/torrents"; # systemd will create this directory for us
+          TempPath = "/srv/torrents/temp";
+          TorrentExportDirectory = "/srv/torrents/.torrents";
+          AlternativeGlobalDLSpeedLimit = 6000;
+          DisableAutoTMMByDefault = false;
+          GlobalMaxRatio = 1; # stop seeding after ratio 1
+          ShareLimitAction = "Stop";
+          QueueingSystemEnabled = false;
+          SubcategoriesEnabled = true;
+        };
+      };
+      Core = {
+        AutoDeleteAddedTorrentFile = "IfAdded";
+      };
+      Preferences = {
+        WebUI = {
+          Username = "${username}";
+          Password_PBKDF2 = "@ByteArray(agI2Tr50yXx8i5Gm9kTfkA==:79jfEujByGcX3FQTbLt2IIm4t7pSxfQhwVIcVFOlTKLtJ1XIJPnDN28+w2udq2ksKpr3UUjxKoCYO6WzaiT+8w==)";
+        };
+      };
+    };
+  };
+
+  # man tmpfiles.d
+  systemd.tmpfiles.rules = [
+    "R /var/lib/qBittorrent/qBittorrent/config/categories.json - - - - -"
+    "C /var/lib/qBittorrent/qBittorrent/config/categories.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/categories.json"
+    "R /var/lib/qBittorrent/qBittorrent/config/watched_folders.json - - - - -"
+    "C /var/lib/qBittorrent/qBittorrent/config/watched_folders.json 0644 qbittorrent qbittorrent - /home/${username}/.dotnix/config/qBittorrent/config/watched_folders.json"
+    "d /srv/torrents 2770 qbittorrent qbittorrent - -"
+  ];
+
+  users.users.${username}.extraGroups = ["qbittorrent"];
+}
