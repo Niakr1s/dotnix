d45f02a9b1cacc52d613ead2dff2c5df4d3fc4bd
Niakr1s <pavel2188@gmail.com>
2026-03-05 12:26:12 +0300

    added aichat to desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -41,4 +41,32 @@ in {
       display-configuration-switcher-shortcuts-enabled = true;
     };
   };
+
+  # aichat
+  programs.aichat = {
+    enable = true;
+
+    settings = {
+      model = "ollama:gemma3:12b"; # Default model config
+      clients = [
+        {
+          type = "openai-compatible";
+          name = "ollama";
+          api_base = "http://localhost:11434/v1";
+          models = [
+            {
+              # should correspond to ollama.nix
+              name = "gemma3:12b";
+
+              # ollama show _model_, under Capabilities section
+              supports_completion = true;
+              supports_function_calling = false;
+              supports_vision = true;
+              supports_reasoning = false;
+            }
+          ];
+        }
+      ];
+    };
+  };
 }
