ead172d0da884279a9c5ff783a709d3316aef9cd
Niakr1s <pavel2188@gmail.com>
2026-03-06 22:32:36 +0300

    added sample disko confituration

---
+++ install/disko.nix
@@ -0,0 +1,96 @@
+# USAGE in your configuration.nix.
+# Update devices to match your hardware.
+# {
+#  imports = [ ./disko-config.nix ];
+#  disko.devices.disk.main.device = "/dev/sda";
+# }
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          # https://nixos.wiki/wiki/ZFS
+          encryption = "on";
+          keyformat = "passphrase";
+          keylocation = "prompt";
+          compression = "zstd";
+          mountpoint = "none";
+          xattr = "sa";
+          acltype = "posixacl";
+          atime = "off";
+        };
+        options.ashift = "12";
+
+        datasets = {
+          "local" = {
+            type = "zfs_fs";
+            options.mountpoint = "none";
+          };
+          "local/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options."com.sun:auto-snapshot" = "false";
+            postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank";
+          };
+          "local/home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            # Used by services.zfs.autoSnapshot options.
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "local/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "local/var" = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "local/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "local/reserved" = {
+            type = "zfs_fs";
+            options = {
+              "com.sun:auto-snapshot" = "false";
+              refreservation = "10G";
+              mountpoint = "none";
+            };
+          };
+        };
+      };
+    };
+  };
+}
