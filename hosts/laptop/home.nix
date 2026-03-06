e2248742635f2e4838a7bc3c0c164adab8232996
Niakr1s <pavel2188@gmail.com>
2026-03-06 11:55:33 +0300

    added osk for laptop

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -18,13 +18,28 @@ in {
     ../default/home.nix
     ./wallpaper.nix # You can change wallpaper in this file
   ];
+  home.packages = with pkgs; [
+    gnomeExtensions.gjs-osk
+  ];
   dconf = {
     enable = true;
     settings = with lib.hm.gvariant; {
       "org/gnome/shell" = {
         enabled-extensions = [
           pkgs.gnomeExtensions.screen-rotate.extensionUuid
+          pkgs.gnomeExtensions.gjs-osk.extensionUuid
         ];
+      };
+      "org/gnome/shell/extensions/gjsosk" = {
+        enable-drag = true;
+        indicator-enabled = true;
+
+        # open upon clicking on text field
+        # 0 = never, 1 = only on touch, 2 = always
+        enable-tap-gesture = 0;
+
+        # default position: bottom corner
+        default-snap = 7;
       };
     };
   };
