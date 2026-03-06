d0700dfbc9813063f5ae0f2847d532a78b5a6db9
Niakr1s <pavel2188@gmail.com>
2026-03-06 13:02:36 +0300

    moved mangohud to modules

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -18,43 +18,10 @@ in {
     ../default/home.nix
     ./wallpaper.nix # You can change wallpaper in this file
     ../../modules/home/aichat/aichat.nix
+    ../../modules/home/mangohud/mangohud.nix
     ../../modules/home/dconf/dconf.multiple.monitors.nix
     ../../modules/home/gnome/extensions/blur-my-shell.nix
   ];
-
-  # mangohud
-  programs.mangohud = {
-    enable = true;
-    settings = {
-      fps_limit = [165 30 60];
-      vsync = 0; # 0 = adaptive; 1 = off; 2 = mailbox; 3 = on
-      time = true;
-      time_no_label = true;
-      time_format = "%F %T";
-      gpu_stats = true;
-      gpu_temp = true;
-      cpu_stats = true;
-      cpu_temp = true;
-      fps = true;
-      frame_timing = 0;
-      font_size = 16;
-      text_outline = true;
-      width = 0;
-      background_alpha = 0.4;
-
-      ### Change toggle keybinds for the hud & logging
-      # toggle_hud=Shift_R+F12
-      # toggle_hud_position=Shift_R+F11
-      # toggle_preset=Shift_R+F10
-      # toggle_fps_limit=Shift_R+F1
-      # toggle_logging=Shift_L+F2
-      # reload_cfg=Shift_L+F4
-      # upload_log=Shift_L+F3
-      # reset_fps_metrics=Shift_R+f9
-      toggle_hud = "Shift_R+F12";
-      toggle_fps_limit = "Shift_R+F1";
-    };
-  };
 
   # lutris
   programs.lutris = with pkgs; {
