3d8ea72084072c5acbb223ab3a60ded9c43c6a81
Niakr1s <pavel2188@gmail.com>
2026-03-06 03:42:01 +0300

    celluloid input.conf to false

--- modules/home/celluloid/celluloid.nix
+++ modules/home/celluloid/celluloid.nix
@@ -19,7 +19,7 @@ in {
 
   dconf.settings = {
     "io/github/celluloid-player/celluloid" = {
-      mpv-input-config-enable = true;
+      mpv-input-config-enable = false; # it won't work with touchpad, so I don't need it
       mpv-input-config-file = "file:///home/${username}/.config/celluloid/input.conf";
       mpv-config-enable = true;
       mpv-config-file = "file:///home/${username}/.config/celluloid/mpv.conf";
