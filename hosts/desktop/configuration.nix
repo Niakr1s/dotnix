bd7846bb4256b9da93158d6bef335953c797f35c
Niakr1s <pavel2188@gmail.com>
2026-03-07 04:19:12 +0300

    refactoring

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -14,20 +14,20 @@
     config = config.nixpkgs.config;
   };
   disko = builtins.fetchTarball {
-    url =  "https://github.com/nix-community/disko/archive/refs/tags/v1.13.0.tar.gz";
+    url = "https://github.com/nix-community/disko/archive/refs/tags/v1.13.0.tar.gz";
     sha256 = "03jz60kw0khm1lp72q65z8gq69bfrqqbj08kw0hbiav1qh3g7p08";
   };
 in {
   imports = [
+    # Don't change this
     ./hardware-configuration.nix
     "${disko}/module.nix"
     ./disko-config.nix
-
     ../default/configuration.nix
     ../../modules/system/hardware/nvidia/nvidia.nix
+    # Don't change this ------- END
 
     # I couldn't wire my gamepad with dongle, so I spent 3 hours to do this...
-    # More in README.md
     # ../../modules/hardware/gamepad/gamepad.nix
 
     ../../modules/system/gaming/gaming.nix
@@ -35,23 +35,26 @@ in {
   ];
 
   # BOOT
-boot = {
-  loader.grub = {
-    enable = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
+  boot = {
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
 
-    # ZFS needs this
-    mirroredBoots = [
-      { devices = [ "nodev" ]; path = "/boot"; }
-    ];
+      # ZFS needs this
+      mirroredBoots = [
+        {
+          devices = ["nodev"];
+          path = "/boot";
+        }
+      ];
+    };
   };
-};
 
-# ZFS need this
-networking.hostId = "82473af6";
+  # ZFS need this
+  networking.hostId = "82473af6";
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages; # ZFS and Nvidia compatibility
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
 
   environment.systemPackages = with pkgs; [
     ### Video editors
