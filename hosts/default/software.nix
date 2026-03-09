907c45a8dffa49f723e56a69fbeb4a450c804c25
Niakr1s <pavel2188@gmail.com>
2026-03-09 10:30:32 +0300

    added some common used programs

--- hosts/default/software.nix
+++ hosts/default/software.nix
@@ -44,6 +44,16 @@ in {
 
   # System packages
   environment.systemPackages = with pkgs; [
+    ((pkgs.ffmpeg-full.override {
+        withUnfree = true; # Allow unfree dependencies (for Nvidia features notably)
+        withMetal = false; # Use Metal API on Mac. Unfree and requires manual downloading of files
+        withMfx = false; # Hardware acceleration via the deprecated intel-media-sdk/libmfx. Use oneVPL instead (enabled by default) from Intel's oneAPI.
+        withTensorflow = false; # Tensorflow dnn backend support (Increases closure size by ~390 MiB)
+        withSmallBuild = true; # Prefer binary size to performance.
+        withDebug = false; # Build using debug options
+      }).overrideAttrs
+      (_: {doCheck = false;}))
+
     vim
     wget
     curl
@@ -59,6 +69,33 @@ in {
     neofetch # for sure
 
     dconf-editor # you probably need it even in not gnome environment
+
+    imagemagick
+    gnupg
+    elinks
+    zellij
+    yt-dlp
+    ytfzf # Posix script to find and watch youtube videos from the terminal
+    jq # lightweight and flexible command-line JSON processor
+    eza # Modern, maintained replacement for ls
+    lazygit # Simple terminal UI for git commands
+    miller # Like awk, sed, cut, join, and sort for data formats such as CSV, TSV, JSON, JSON Lines, and positionally-indexed
+    visidata # Interactive terminal multitool for tabular data
+    delta # Syntax-highlighting pager for git
+    parallel # Shell tool for executing jobs in parallel
+    duf # Disk Usage/Free Utility
+    ncdu # Disk usage analyzer with an ncurses interface
+    dua # Tool to conveniently learn about the disk usage of directories
+    qalculate-gtk # calculator
+    calc # C-style arbitrary precision calculator
+    zoxide # Fast cd command that learns your habits
+    fortune # Program that displays a pseudorandom message from a database of quotations
+    cowsay
+    lolcat # Rainbow version of cat
+    mutt # Small but very powerful text-based mail client
+    rhash # Console utility and library for computing and verifying hash sums of files
+    rclone # Command line program to sync files and directories to and from major cloud storage
+    rsync # Fast incremental file transfer utility
   ];
 
   # Home packages
