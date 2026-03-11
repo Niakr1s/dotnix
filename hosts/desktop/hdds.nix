76dd3109ebd47d24d1cbcc8e62e6aa5f92c88387
Niakr1s <pavel2188@gmail.com>
2026-03-11 11:09:29 +0300

    simplified hdds.nix

--- hosts/desktop/hdds.nix
+++ hosts/desktop/hdds.nix
@@ -35,8 +35,5 @@
     (mkHdd "sdb2" "desktop/veracrypt/hdd4_pass" "hdd4" "/data/hdd4")
     (mkHdd "sdd2" "desktop/veracrypt/hdd20_pass" "hdd20" "/data/hdd20")
   ];
-in {
-  sops.secrets = lib.mkMerge (map (cfg: cfg.sops.secrets) hddConfigs);
-  home-manager.users.${username} = lib.mkMerge (map (cfg: cfg.home-manager.users.${username}) hddConfigs);
-  systemd.tmpfiles.rules = lib.mkMerge (map (cfg: cfg.systemd.tmpfiles.rules) hddConfigs);
-}
+in
+  lib.foldl lib.recursiveUpdate {} hddConfigs
