5d0b21ac708412f114ddba7f8cf29d46d7db2d6b
Niakr1s <pavel2188@gmail.com>
2026-03-11 16:52:33 +0300

    some fiddle with nix configs

--- modules/video/losslesscut/losslesscut.nix
+++ modules/video/losslesscut/losslesscut.nix
@@ -9,11 +9,16 @@
   username,
   flakeDir,
   ...
-}: let
-  losslesscut_conf = builtins.readFile ./../../../config/LosslessCut/config.json;
-  losslesscut_text = lib.replaceStrings ["\n"] ["\\n"] "${losslesscut_conf}";
-in {
+}: {
   environment.systemPackages = with pkgs; [
     losslesscut-bin
   ];
+
+  # It works, but maybe it will be better to manually copy a needed file
+  # home-manager.users.${username} = {lib, ...}: {
+  #   home.activation.installLosslessCutConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #     mkdir -p /home/${username}/.config/LosslessCut
+  #     cp -f ${flakeDir}/config/LosslessCut/* /home/${username}/.config/LosslessCut/
+  #   '';
+  # };
 }
