e1492e9eaea9d585884909ac489509b83953f8e4
Niakr1s <pavel2188@gmail.com>
2026-03-11 11:03:43 +0300

    completed hdds.nix

--- hosts/desktop/hdds.nix
+++ hosts/desktop/hdds.nix
@@ -22,10 +22,14 @@
 
     home-manager.users.${username} = {
       home.shellAliases = {
-        "${aliasName}" = "cat /run/secrets/${sopsPath} | veracrypt --text --stdin --non-interactive --pim=0 --protect-hidden=no /dev/${devName} ${mountPoint}";
+        "${aliasName}m" = "cat /run/secrets/${sopsPath} | veracrypt --text --stdin --non-interactive --pim=0 --protect-hidden=no /dev/${devName} ${mountPoint}";
         "${aliasName}u" = "veracrypt -d ${mountPoint}";
+        "${aliasName}cd" = "cd ${mountPoint}";
       };
     };
+    systemd.tmpfiles.rules = [
+      "d ${mountPoint} 700 ${username} users - -"
+    ];
   };
   hddConfigs = [
     (mkHdd "sdb2" "desktop/veracrypt/hdd4_pass" "hdd4" "/data/hdd4")
@@ -34,4 +38,5 @@
 in {
   sops.secrets = lib.mkMerge (map (cfg: cfg.sops.secrets) hddConfigs);
   home-manager.users.${username} = lib.mkMerge (map (cfg: cfg.home-manager.users.${username}) hddConfigs);
+  systemd.tmpfiles.rules = lib.mkMerge (map (cfg: cfg.systemd.tmpfiles.rules) hddConfigs);
 }
