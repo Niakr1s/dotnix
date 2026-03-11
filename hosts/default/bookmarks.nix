af963eafa02fd7a8cb31b3cef6eef9c9e96a4aaf
Niakr1s <pavel2188@gmail.com>
2026-03-11 13:59:29 +0300

    moved around bookmarks

--- hosts/default/bookmarks.nix
+++ hosts/default/bookmarks.nix
@@ -15,12 +15,13 @@
       gtk3 = {
         enable = true;
         bookmarks = [
+          "file:/// root"
+          "file:///srv/torrents torrents"
           "file:///home/${username}/Music Music"
           "file:///home/${username}/Videos Videos"
           "file:///home/${username}/Pictures Pictures"
           "file:///home/${username}/Documents Documents"
           "file:///home/${username}/Downloads Downloads"
-          "file:///srv/torrents torrents"
         ];
       };
     };
