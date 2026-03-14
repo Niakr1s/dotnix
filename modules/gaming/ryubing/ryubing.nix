4ef39c65c7662f90bbef01d856ff2a2ca4c961fd
Niakr1s <pavel2188@gmail.com>
2026-03-14 22:08:52 +0300

    reorganized ryu

--- modules/gaming/ryubing/ryubing.nix
+++ modules/gaming/ryubing/ryubing.nix
@@ -9,21 +9,26 @@
   home-manager,
   flakeDir,
   ...
-}: {
+}: let
+  prodKeysFile = "prod.v21.2.0.keys";
+in {
   environment.systemPackages = with pkgs; [
     ryubing
     nsz
   ];
 
   home-manager.users.${username} = {config, ...}: {
+    # ryujinx config
     home.file.".config/Ryujinx/system/prod.keys" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.switch/prod.keys";
+      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
     };
     home.file.".config/Ryujinx/Config.json" = {
       source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/Config.json";
     };
+
+    # nsz config
     home.file.".switch/prod.keys" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.switch/prod.keys";
+      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
     };
   };
 }
