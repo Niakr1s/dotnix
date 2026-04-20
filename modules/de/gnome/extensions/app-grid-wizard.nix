0790c626e86f897c4d6d7476375c6c9b9e07080f
Niakr1s <pavel2188@gmail.com>
2026-04-20 14:09:01 +0300

    app grid wizard

---
+++ modules/de/gnome/extensions/app-grid-wizard.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  nixpkgs-unstable,
+  hostname,
+  username,
+  home-manager,
+  ...
+}: let
+in {
+  home-manager.users.${username} = {
+    home.packages = with pkgs; [
+      gnomeExtensions.app-grid-wizard
+    ];
+    dconf.settings = {
+    };
+  };
+}
--- modules/de/gnome/gnome.nix
+++ modules/de/gnome/gnome.nix
@@ -10,6 +10,7 @@
 }: {
   imports = [
     ./extensions/gsconnect.nix
+    ./extensions/app-grid-wizard.nix
   ];
 
   services.desktopManager.gnome.enable = true;
