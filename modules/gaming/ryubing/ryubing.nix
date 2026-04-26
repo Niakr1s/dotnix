9932c6f3b494d3e7dc1fca45f791e28f785db4f4
Niakr1s <pavel2188@gmail.com>
2026-04-27 01:05:52 +0300

    ryujinx fix with gnome-session-inhibit (untested)

--- modules/gaming/ryubing/ryubing.nix
+++ modules/gaming/ryubing/ryubing.nix
@@ -11,6 +11,14 @@
   ...
 }: let
   prodKeysFile = "prod.v21.2.0.keys";
+
+  # Read the contents of the original desktop file provided by the package
+  originalDesktopFile = builtins.readFile "${pkgs.ryubing}/share/applications/Ryujinx.desktop";
+  # Create a modified version with the new Exec line
+  modifiedDesktopFile = pkgs.runCommand "modified-ryujinx-desktop" {} ''
+    echo "${originalDesktopFile}" > $out
+    sed -i 's|^Exec=\(.*\)|Exec=gnome-session-inhibit --reason=Ryujinx --app-id=ryujinx \1|' $out
+  '';
 in {
   environment.systemPackages = with pkgs; [
     ryubing
@@ -18,6 +26,11 @@ in {
   ];
 
   home-manager.users.${username} = {config, ...}: {
+    home.file.".local/share/applications/Ryujinx.desktop" = {
+      text = builtins.readFile modifiedDesktopFile;
+      force = true;
+    };
+
     # for nsz
     home.file.".switch/prod.keys" = {
       source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
