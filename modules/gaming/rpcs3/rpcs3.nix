d079228a916cbca0bd6c15d602ac2eb1b31a1d82
Niakr1s <pavel2188@gmail.com>
2026-04-28 23:42:15 +0300

    fixed rpcs3 configs (but rpcs3 still doesn't respects config symlinks)

--- modules/gaming/rpcs3/rpcs3.nix
+++ modules/gaming/rpcs3/rpcs3.nix
@@ -18,6 +18,7 @@ in {
   home-manager.users.${username} = {config, ...}: {
     home.file.".config/rpcs3/config.yml" = {
       source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/rpcs3/config.yml";
+      force = true;
     };
     home.file.".config/rpcs3/custom_configs" = {
       source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/rpcs3/custom_configs";
