ae1bb9e3e58f6e3772c963a669bd0fffa0b7d98b
Niakr1s <pavel2188@gmail.com>
2026-05-10 18:08:14 +0300

    added luksUmounts

--- hosts/desktop/aliases.nix
+++ hosts/desktop/aliases.nix
@@ -11,6 +11,16 @@
     (luksMount "3b5448d2-3780-4cee-8529-caec12d1055e" "hdd20")
     (luksMount "fabfb454-6de2-412d-8bc7-b56a849c77c7" "hdd24")
   ];
+
+  luksUmount = label: {
+    home-manager.users.${username}.home.shellAliases."${label}u" = "sudo umount /data/${label} && sudo cryptsetup luksClose ${label}";
+  };
+  luksUmounts = [
+    (luksUmount "hdd4")
+    (luksUmount "hdd20")
+    (luksUmount "hdd24")
+  ];
 in
-  lib.foldl lib.recursiveUpdate {}
-  luksMounts
+  lib.foldl lib.recursiveUpdate
+  {}
+  (luksMounts ++ luksUmounts)
