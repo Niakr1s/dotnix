c694792341e92715b3970ba73ebc5d1227528adf
Niakr1s <pavel2188@gmail.com>
2026-05-13 09:01:09 +0300

    docker nvidia cdi

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,52 +1,56 @@
 {config, ...}: {
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
-    # You probably want stable or beta.
-    package = config.boot.kernelPackages.nvidiaPackages.stable; # Same as production
-    # package = config.boot.kernelPackages.nvidiaPackages.production; # Latest production driver
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;   # Latest beta driver
-    # package = config.boot.kernelPackages.nvidiaPackages.latest; # Same as production
-    # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-    # package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
-    # package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-    # package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
-    # package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
-    # package = config.boot.kernelPackages.nvidiaPackages.dc; # Datacenter drivers
-    # package = config.boot.kernelPackages.nvidiaPackages.dc_565;
-    # package = config.boot.kernelPackages.nvidiaPackages.dc_535;
+  hardware = {
+    # Enable OpenGL
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
 
-    # Modesetting is required.
-    modesetting.enable = true;
+    nvidia-container-toolkit.enable = true; # for docker
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
+    nvidia = {
+      # You probably want stable or beta.
+      package = config.boot.kernelPackages.nvidiaPackages.stable; # Same as production
+      # package = config.boot.kernelPackages.nvidiaPackages.production; # Latest production driver
+      # package = config.boot.kernelPackages.nvidiaPackages.beta;   # Latest beta driver
+      # package = config.boot.kernelPackages.nvidiaPackages.latest; # Same as production
+      # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+      # package = config.boot.kernelPackages.nvidiaPackages.legacy_535; # Older versions
+      # package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+      # package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+      # package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
+      # package = config.boot.kernelPackages.nvidiaPackages.dc; # Datacenter drivers
+      # package = config.boot.kernelPackages.nvidiaPackages.dc_565;
+      # package = config.boot.kernelPackages.nvidiaPackages.dc_535;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+      # Modesetting is required.
+      modesetting.enable = true;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # If you have an GPU with Turing architecture (RTX 20-Series) or newer set hardware.nvidia.open to true.
-    open = true;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # If you have an GPU with Turing architecture (RTX 20-Series) or newer set hardware.nvidia.open to true.
+      open = true;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+    };
   };
 }
--- modules/vm/docker.nix
+++ modules/vm/docker.nix
@@ -7,6 +7,31 @@
       setSocketVariable = true;
       # Customize Docker daemon settings using the daemon.settings option
       daemon.settings = {
+        # You may also need to adjust your docker compose file to use cdi instead of the nvidia driver.
+        # services:
+        #   ollama:
+        #     image: ollama/ollama
+        #     volumes:
+        #       - ollama:/root/.ollama
+        #     ports:
+        #       - 11434:11434
+        #     deploy:
+        #       resources:
+        #         reservations:
+        #           devices:
+        #             # Go from this:
+        #             # - driver: nvidia
+        #             #   count: all
+        #             #   capabilities: [gpu]
+        #             # To this:
+        #             - driver: cdi
+        #               capabilities: [gpu]
+        #               device_ids:
+        #                 - nvidia.com/gpu=all
+        # volumes:
+        #   ollama: {}
+        features.cdi = true; # for gpu accelleration
+
         dns = ["1.1.1.1" "8.8.8.8"];
         log-driver = "journald";
         registry-mirrors = ["https://mirror.gcr.io"];
