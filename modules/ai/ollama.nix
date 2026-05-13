5aec343d3439550bcb3badbeedae73a11cfe8844
Niakr1s <pavel2188@gmail.com>
2026-05-13 12:26:12 +0300

    added some models

--- modules/ai/ollama.nix
+++ modules/ai/ollama.nix
@@ -3,9 +3,10 @@
     enable = true;
     package = pkgs.ollama-cuda;
     loadModels = [
-      "gemma3:12b"
+      "gemma4:e4b"
       "qwen2.5-coder:7b"
       "qwen2.5-coder:14b"
+      "qwen3.5:9b"
     ];
   };
 }
