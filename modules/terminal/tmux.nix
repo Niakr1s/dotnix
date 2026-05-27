27333e39f5a6a24852468325fba0904e284f4a50
Niakr1s <pavel2188@gmail.com>
2026-05-27 21:22:26 +0300

    added tmux resize pane hotkeys

--- modules/terminal/tmux.nix
+++ modules/terminal/tmux.nix
@@ -26,6 +26,12 @@
       bind k select-pane -U
       bind l select-pane -R
 
+      # Use Alt+hjkl for immediate resizing
+      bind -r H resize-pane -L 5
+      bind -r J resize-pane -D 5
+      bind -r K resize-pane -U 5
+      bind -r L resize-pane -R 5
+
       # restore on tmux server start
       set -g @continuum-restore 'on'
 
