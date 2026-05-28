ecf54c09390c3b11c336180f08f3b97f68c8b325
Niakr1s <pavel2188@gmail.com>
2026-05-29 00:30:44 +0300

    avahi denyInterfaces

--- modules/services/avahi.nix
+++ modules/services/avahi.nix
@@ -5,7 +5,10 @@
     nssmdns4 = true; # Integrates .local resolution with your system's DNS
     openFirewall = true;
     wideArea = true;
-    allowInterfaces = ["eno1"];
+    denyInterfaces = [
+      "virbr0"
+      "docker0"
+    ];
     publish = {
       enable = true; # Allows services to be published
       addresses = true; # Publishes the machine's IP addresses
