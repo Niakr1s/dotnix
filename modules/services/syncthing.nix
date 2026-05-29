9f5de8d2e7db11ab35129135bc8834fe35049e66
Niakr1s <pavel2188@gmail.com>
2026-05-29 04:47:19 +0300

    syncthing: from mkMerge to imports

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -12,41 +12,42 @@ let
     lib.last (lib.strings.splitString ":" config.services.syncthing.guiAddress)
   );
 in
-lib.mkMerge [
-  (flakeLib.localhostReverseProxy "syncthing" port)
-  {
-    sops.secrets = {
-      # This is the actual specification of the secrets.
-      "${sopsPath}" = {
-        mode = "0440";
-        owner = config.users.users.${username}.name;
-        group = config.users.users.${username}.group;
-      };
-    };
-    services.syncthing = {
-      enable = true;
-      openDefaultPorts = true; # Open ports in the firewall for Syncthing.
-      guiPasswordFile = "/run/secrets/common/syncthing";
-      settings = {
-        gui.user = "${username}";
+{
+  imports = [
+    (flakeLib.localhostReverseProxy "syncthing" port)
+  ];
 
-        # TODO: configure this maybe
-        # devices = {
-        #   "device1" = {id = "DEVICE-ID-GOES-HERE";};
-        #   "device2" = {id = "DEVICE-ID-GOES-HERE";};
-        # };
-        # folders = {
-        #   "Documents" = {
-        #     path = "/home/myusername/Documents";
-        #     devices = ["device1" "device2"];
-        #   };
-        #   "Example" = {
-        #     path = "/home/myusername/Example";
-        #     devices = ["device1"];
-        #     ignorePerms = false; # Enable file permission syncing
-        #   };
-        # };
-      };
+  sops.secrets = {
+    # This is the actual specification of the secrets.
+    "${sopsPath}" = {
+      mode = "0440";
+      owner = config.users.users.${username}.name;
+      group = config.users.users.${username}.group;
     };
-  }
-]
+  };
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true; # Open ports in the firewall for Syncthing.
+    guiPasswordFile = "/run/secrets/common/syncthing";
+    settings = {
+      gui.user = "${username}";
+
+      # TODO: configure this maybe
+      # devices = {
+      #   "device1" = {id = "DEVICE-ID-GOES-HERE";};
+      #   "device2" = {id = "DEVICE-ID-GOES-HERE";};
+      # };
+      # folders = {
+      #   "Documents" = {
+      #     path = "/home/myusername/Documents";
+      #     devices = ["device1" "device2"];
+      #   };
+      #   "Example" = {
+      #     path = "/home/myusername/Example";
+      #     devices = ["device1"];
+      #     ignorePerms = false; # Enable file permission syncing
+      #   };
+      # };
+    };
+  };
+}
