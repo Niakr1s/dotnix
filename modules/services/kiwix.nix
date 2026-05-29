eba40b3e590e466c9df8891110ea5f75c40b0254
Niakr1s <pavel2188@gmail.com>
2026-05-29 04:45:31 +0300

    simplified kiwix

--- modules/services/kiwix.nix
+++ modules/services/kiwix.nix
@@ -1,26 +1,22 @@
-{inputs, ...}: let
+{
+  lib,
+  inputs,
+  config,
+  flakeLib,
+  ...
+}:
+let
   libraryPath = "/data/hdd1/kiwix/library.xml";
-in {
-  imports = ["${inputs.nixpkgs-unstable}/nixos/modules/services/misc/kiwix-serve.nix"];
+in
+{
+  imports = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/kiwix-serve.nix"
+    (flakeLib.localhostReverseProxy "kiwix" config.services.kiwix-serve.port)
+  ];
 
   services.kiwix-serve = {
     enable = true;
     port = 7777;
     libraryPath = "${libraryPath}";
-  };
-
-  services.caddy = {
-    enable = true;
-    virtualHosts."http://kiwix.local" = {
-      extraConfig = ''
-        reverse_proxy localhost:7777
-      '';
-    };
-  };
-
-  networking.hosts = {
-    "127.0.0.1" = [
-      "kiwix.local"
-    ];
   };
 }
