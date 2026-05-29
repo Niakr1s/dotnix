b0bbd77f77f075daaf7da7a2e6979be6d1c94308
Niakr1s <pavel2188@gmail.com>
2026-05-29 12:27:54 +0300

    added desktop entry for shadps4-qtlauncher-desktop

--- modules/gaming/shadps4.nix
+++ modules/gaming/shadps4.nix
@@ -18,10 +18,30 @@ let
     in
     appimageTools.wrapType2 { inherit pname version src; }
   ) { };
+
+  icon = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/shadps4-emu/shadps4-qtlauncher/refs/tags/v224/.github/shadps4.png";
+    hash = "sha256-rya0oTnFzS0D48YcB18AevMw//q+8t3sUQwjDC9LR5U=";
+  };
+
+  shadps4-qtlauncher-desktop = pkgs.makeDesktopItem {
+    name = "shadps4-qtlauncher";
+    exec = "${shadps4-qtlauncher}/bin/shadps4-qtlauncher";
+    icon = "${icon}"; # Adjust path if icon exists
+    desktopName = "shadPS4 Qt Launcher";
+    comment = "Qt Launcher for shadPS4 Emulator";
+    categories = [
+      "Game"
+      "Emulator"
+    ];
+    terminal = false;
+    startupNotify = true;
+  };
 in
 {
   environment.systemPackages = with pkgs; [
     unstable.shadps4
     shadps4-qtlauncher
+    shadps4-qtlauncher-desktop
   ];
 }
