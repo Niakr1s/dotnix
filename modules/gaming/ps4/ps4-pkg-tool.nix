37114471174ceb6e79a31c20f4efa9c3720fefe9
Niakr1s <pavel2188@gmail.com>
2026-05-29 21:27:39 +0300

    added lib.warn to ps4-pkg-tool

--- modules/gaming/ps4/ps4-pkg-tool.nix
+++ modules/gaming/ps4/ps4-pkg-tool.nix
@@ -1,26 +1,32 @@
-{ pkgs, ... }:
-pkgs.stdenv.mkDerivation rec {
-  pname = "ps4-pkg-tool";
-  version = "20250825-123142-e7c40358";
+{
+  pkgs,
+  lib,
+  ...
+}:
+lib.warn "ps4-pkg-tools can't properly extract patches, so use PKGInstall package"
+  pkgs.stdenv.mkDerivation
+  rec {
+    pname = "ps4-pkg-tool";
+    version = "20250825-123142-e7c40358";
 
-  src = pkgs.fetchzip {
-    url = "https://github.com/xXJSONDeruloXx/ps4-pkg-tools/releases/download/v${version}/ps4-pkg-tools-Linux.tar.gz";
-    hash = "sha256-DV+3a2QdBJhEhV6YKd4oPIynjrZ1Evu88Wu+e2W4A+s=";
-    stripRoot = false;
-  };
+    src = pkgs.fetchzip {
+      url = "https://github.com/xXJSONDeruloXx/ps4-pkg-tools/releases/download/v${version}/ps4-pkg-tools-Linux.tar.gz";
+      hash = "sha256-DV+3a2QdBJhEhV6YKd4oPIynjrZ1Evu88Wu+e2W4A+s=";
+      stripRoot = false;
+    };
 
-  # Add Qt library and wrapping hook
-  buildInputs = [
-    pkgs.qt6.qtbase
-  ];
+    # Add Qt library and wrapping hook
+    buildInputs = [
+      pkgs.qt6.qtbase
+    ];
 
-  nativeBuildInputs = [
-    pkgs.autoPatchelfHook # Fixes library paths for prebuilt binaries
-    pkgs.qt6.wrapQtAppsHook # Wraps Qt apps with correct environment
-  ];
+    nativeBuildInputs = [
+      pkgs.autoPatchelfHook # Fixes library paths for prebuilt binaries
+      pkgs.qt6.wrapQtAppsHook # Wraps Qt apps with correct environment
+    ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r * $out/bin/
-  '';
-}
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r * $out/bin/
+    '';
+  }
