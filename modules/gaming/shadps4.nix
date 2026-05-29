f2ccc70c1b68e4e77032d9b3afbad1efdd860149
Niakr1s <pavel2188@gmail.com>
2026-05-29 21:09:23 +0300

    added PKGInstall tool

--- modules/gaming/shadps4.nix
+++ modules/gaming/shadps4.nix
@@ -13,6 +13,34 @@ let
   };
 
   # Launcher
+  PKGInstall = pkgs.stdenv.mkDerivation rec {
+    pname = "PKGInstall";
+    version = "2025-10-24-96c7890";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/Muggle345/PKGInstall/releases/download/Release/PKGInstall-ubuntu64-${version}.zip";
+      hash = "sha256-9ZTWFggwz6Rr3Raa5aABcvOMOt7flUKBbJWK/AIToCQ=";
+    };
+
+    buildInputs = with pkgs.qt6; [
+      qtbase
+      qtdeclarative
+      qtmultimedia
+      qttools
+    ];
+
+    nativeBuildInputs = with pkgs.qt6; [
+      wrapQtAppsHook
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp PKGInstall $out/bin/
+      chmod +x $out/bin/*
+    '';
+  };
+
+  # Launcher
   shadps4-qtlauncher = pkgs.callPackage (
     { appimageTools, fetchzip }:
     let
@@ -76,5 +104,6 @@ in
     shadps4-qtlauncher
     shadps4-qtlauncher-desktop
     ps4-pkg-tool
+    PKGInstall
   ];
 }
