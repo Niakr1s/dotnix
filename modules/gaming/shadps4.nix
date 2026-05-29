a8c43aa1842c2a4dd54fb6035583455579fefa6a
Niakr1s <pavel2188@gmail.com>
2026-05-29 12:11:43 +0300

    added shadps4-qtlauncher (no cli)

--- modules/gaming/shadps4.nix
+++ modules/gaming/shadps4.nix
@@ -2,8 +2,26 @@
   pkgs,
   ...
 }:
+let
+  shadps4-qtlauncher = pkgs.callPackage (
+    { appimageTools, fetchzip }:
+    let
+      pname = "shadps4-qtlauncher";
+      version = "224";
+
+      extracted = fetchzip {
+        url = "https://github.com/shadps4-emu/shadps4-qtlauncher/releases/download/v224/shadPS4QtLauncher-linux-qt-v${version}.zip";
+        hash = "sha256-/h/ZPZ2c1WehizJWY719r8OXhZk0UO9/hr6fxtiK7mo=";
+      };
+
+      src = "${extracted}/shadPS4QtLauncher-qt.AppImage";
+    in
+    appimageTools.wrapType2 { inherit pname version src; }
+  ) { };
+in
 {
   environment.systemPackages = with pkgs; [
     unstable.shadps4
+    shadps4-qtlauncher
   ];
 }
