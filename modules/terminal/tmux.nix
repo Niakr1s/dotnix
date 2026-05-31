738769da19aa3b43afc4c61c916b5b7ecee152bf
Niakr1s <pavel2188@gmail.com>
2026-05-31 11:42:30 +0300

    tmux select pane and resize pane hotkeys

--- modules/terminal/tmux.nix
+++ modules/terminal/tmux.nix
@@ -26,11 +26,23 @@
       bind k select-pane -U
       bind l select-pane -R
 
+      # Direct pane movement with Alt+hjkl (no prefix needed)
+      bind -n M-h select-pane -L
+      bind -n M-j select-pane -D
+      bind -n M-k select-pane -U
+      bind -n M-l select-pane -R
+
       # Use Alt+hjkl for immediate resizing
-      bind -r C-h resize-pane -L 5
-      bind -r C-j resize-pane -D 5
-      bind -r C-k resize-pane -U 5
-      bind -r C-l resize-pane -R 5
+      bind -r M-h resize-pane -L 10
+      bind -r M-j resize-pane -D 10
+      bind -r M-k resize-pane -U 10
+      bind -r M-l resize-pane -R 10
+
+      # Unbind pane resizing
+      unbind C-h
+      unbind C-j
+      unbind C-k
+      unbind C-l
 
       # restore on tmux server start
       set -g @continuum-restore 'on'
