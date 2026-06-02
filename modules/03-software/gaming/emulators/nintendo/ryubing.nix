77aa0e79fe0044b65b297790d9b5edecae2c90be
Niakr1s <pavel2188@gmail.com>
2026-06-02 11:10:30 +0300

    added ryujinx cheats

--- modules/03-software/gaming/emulators/nintendo/ryubing.nix
+++ modules/03-software/gaming/emulators/nintendo/ryubing.nix
@@ -3,13 +3,14 @@
   username,
   flakeDir,
   ...
-}: let
+}:
+let
   prodKeysFile = "prod.v21.2.0.keys";
 
   # Read the contents of the original desktop file provided by the package
   originalDesktopFile = builtins.readFile "${pkgs.ryubing}/share/applications/Ryujinx.desktop";
   # Create a modified version with the new Exec line
-  modifiedDesktopFile = pkgs.runCommand "modified-ryujinx-desktop" {} ''
+  modifiedDesktopFile = pkgs.runCommand "modified-ryujinx-desktop" { } ''
     echo "${originalDesktopFile}" > $out
     sed -i 's|^Exec=\(.*\)|Exec=systemd-inhibit \1|' $out
   '';
@@ -19,31 +20,35 @@
     url = "https://github.com/HamletDuFromage/switch-cheats-db/releases/download/2026-04-18/contents_complete.zip";
     hash = "sha256-PxsMHOKWcmD6yZX9jxKo2aGtUWg8w1JT3h6sgu1QJSo=";
   };
-in {
+in
+{
   environment.systemPackages = with pkgs; [
     ryubing
     nsz
   ];
 
-  home-manager.users.${username} = {config, ...}: {
-    home.file.".local/share/applications/Ryujinx.desktop" = {
-      text = builtins.readFile modifiedDesktopFile;
-      force = true;
-    };
+  home-manager.users.${username} =
+    { config, ... }:
+    {
+      home.file.".local/share/applications/Ryujinx.desktop" = {
+        text = builtins.readFile modifiedDesktopFile;
+        force = true;
+      };
 
-    # for nsz
-    home.file.".switch/prod.keys" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
-    };
+      # for nsz
+      home.file.".switch/prod.keys" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
+      };
 
-    home.file.".config/Ryujinx/system/prod.keys" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
+      home.file.".config/Ryujinx/system/prod.keys" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/system/${prodKeysFile}";
+      };
+      home.file.".config/Ryujinx/Config.json" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/Config.json";
+      };
+      home.file.".config/Ryujinx/mods/contents" = {
+        source = "${cheats}";
+        recursive = true;
+      };
     };
-    home.file.".config/Ryujinx/Config.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/Ryujinx/Config.json";
-    };
-    home.file.".config/Ryujinx/mods/contents" = {
-      source = "${cheats}";
-    };
-  };
 }
