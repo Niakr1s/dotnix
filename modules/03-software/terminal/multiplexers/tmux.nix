bdad8cfca03bdaf470d7cc96c53c0f77d4891de7
Niakr1s <pavel2188@gmail.com>
2026-06-05 09:38:23 +0300

    added vim-tmux-navigator

--- modules/03-software/terminal/multiplexers/tmux.nix
+++ modules/03-software/terminal/multiplexers/tmux.nix
@@ -12,6 +12,7 @@
       continuum
       yank
       open
+      vim-tmux-navigator
     ];
 
     extraConfig = ''
@@ -20,29 +21,11 @@
       unbind C-b
       set -g prefix C-q
 
-      # Pane navigation with prefix + hjkl
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # Direct pane movement with Alt+hjkl (no prefix needed)
-      bind -n M-h select-pane -L
-      bind -n M-j select-pane -D
-      bind -n M-k select-pane -U
-      bind -n M-l select-pane -R
-
       # Use Alt+hjkl for immediate resizing
       bind -r M-h resize-pane -L 10
       bind -r M-j resize-pane -D 10
       bind -r M-k resize-pane -U 10
       bind -r M-l resize-pane -R 10
-
-      # Unbind pane resizing
-      unbind C-h
-      unbind C-j
-      unbind C-k
-      unbind C-l
 
       # restore on tmux server start
       set -g @continuum-restore 'on'
