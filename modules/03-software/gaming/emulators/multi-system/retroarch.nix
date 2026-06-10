3052ee28c9b371f40540acb9a685c3b1f70e9588
Niakr1s <pavel2188@gmail.com>
2026-06-10 20:40:04 +0300

    enabled retroarch full

--- modules/03-software/gaming/emulators/multi-system/retroarch.nix
+++ modules/03-software/gaming/emulators/multi-system/retroarch.nix
@@ -5,30 +5,30 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    # retroarch-full
+    retroarch-full
     # retroarch
-    (retroarch.withCores (cores:
-      with cores; [
-        fbneo # Arcade - Fbneo
-        mame2003-plus # Arcade - Mame 2003 Plus
-        stella # Atari - 2600
-        beetle-pce-fast # NEC - TurboGrafx-16 / TurboGrafx CD
-        citra # Nintendo - 3DS
-        melonds # Nintendo - DS
-        gambatte # Nintendo - GB / GB Color
-        mgba # Nintendo - GBA
-        dolphin # Nintendo - GameCube / Wii
-        mupen64plus # Nintendo -N64
-        nestopia # Nintendo - NES
-        bsnes # Nintendo - SNES
-        scummvm # ScummVM
-        flycast # Sega - Dreamcast
-        genesis-plus-gx # Sega - Game Gear / Genesis / Master System / Sega CD
-        beetle-saturn # Sega - Saturn
-        neocd # SNK - NEO GEO
-        beetle-psx # Sony - PS1
-        pcsx2 # Sony - PS2
-      ]))
+    # (retroarch.withCores (cores:
+    #   with cores; [
+    #     fbneo # Arcade - Fbneo
+    #     mame2003-plus # Arcade - Mame 2003 Plus
+    #     stella # Atari - 2600
+    #     beetle-pce-fast # NEC - TurboGrafx-16 / TurboGrafx CD
+    #     citra # Nintendo - 3DS
+    #     melonds # Nintendo - DS
+    #     gambatte # Nintendo - GB / GB Color
+    #     mgba # Nintendo - GBA
+    #     dolphin # Nintendo - GameCube / Wii
+    #     mupen64plus # Nintendo -N64
+    #     nestopia # Nintendo - NES
+    #     bsnes # Nintendo - SNES
+    #     scummvm # ScummVM
+    #     flycast # Sega - Dreamcast
+    #     genesis-plus-gx # Sega - Game Gear / Genesis / Master System / Sega CD
+    #     beetle-saturn # Sega - Saturn
+    #     neocd # SNK - NEO GEO
+    #     beetle-psx # Sony - PS1
+    #     pcsx2 # Sony - PS2
+    #   ]))
   ];
 
   # home-manager.users.${username} = {config, ...}: {
