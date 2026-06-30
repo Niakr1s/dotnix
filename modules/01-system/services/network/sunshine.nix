c1742f53337d0441cb0534e203a27199faff68c0
Niakr1s <pavel2188@gmail.com>
2026-06-30 13:48:35 +0300

    sunshine use cuda for desktop

--- modules/01-system/services/network/sunshine.nix
+++ modules/01-system/services/network/sunshine.nix
@@ -1,26 +1,24 @@
 {
-  flakeLib,
   pkgs,
+  flakeLib,
+  hostname,
   ...
 }:
 let
   port = 47989;
   guiPort = port + 1;
+
+  sunshinePackage =
+    if hostname == "desktop" then pkgs.sunshine.override { cudaSupport = true; } else pkgs.sunshine;
 in
 {
   imports = [
     (flakeLib.localhostReverseProxy "sunshine" guiPort)
   ];
 
-  environment.systemPackages = with pkgs; [
-    sunshine
-  ];
-
   services.sunshine = {
     enable = true;
-    package = pkgs.sunshine.override {
-      cudaSupport = false;
-    };
+    package = sunshinePackage;
     autoStart = true;
     openFirewall = true;
     capSysAdmin = true;
