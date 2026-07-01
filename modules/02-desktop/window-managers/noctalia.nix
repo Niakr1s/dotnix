733e22b02945f8c87691b28140be9f0d2802367e
Niakr1s <pavel2188@gmail.com>
2026-07-01 14:31:21 +0300

    reverted swayidle for noctalia

--- modules/02-desktop/window-managers/noctalia.nix
+++ modules/02-desktop/window-managers/noctalia.nix
@@ -5,6 +5,7 @@
 {
   pkgs,
   username,
+  hostname,
   flakeDir,
   ...
 }:
@@ -30,38 +31,38 @@
   home-manager.users.${username} =
     { config, ... }:
     {
-      # services.swayidle =
-      #   let
-      #     lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call sessionMenu lockAndSuspend";
-      #     display = status: "${pkgs.wlopm}/bin/wlopm --${status} '*'";
-      #     lockTimeout = if hostname == "laptop" then 300 else 600;
-      #     displayOffTimeout = 20;
-      #   in
-      #   {
-      #     enable = true;
-      #     timeouts = [
-      #       # lock and suspend
-      #       {
-      #         timeout = lockTimeout;
-      #         command = lock;
-      #         resumeCommand = display "on";
-      #       }
-      #
-      #       # turn off display after lock (for hosts where suspend disabled)
-      #       {
-      #         timeout = lockTimeout + displayOffTimeout;
-      #         command = display "off";
-      #         resumeCommand = display "on";
-      #       }
-      #
-      #       # turn off display while idle on lockscreen
-      #       {
-      #         timeout = displayOffTimeout;
-      #         command = "${pkgs.procps}/bin/pgrep gtklock && { ${lock}; ${display "off"}; }; }";
-      #         resumeCommand = display "on";
-      #       }
-      #     ];
-      #   };
+      services.swayidle =
+        let
+          lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call sessionMenu lockAndSuspend";
+          display = status: "${pkgs.wlopm}/bin/wlopm --${status} '*'";
+          lockTimeout = if hostname == "laptop" then 300 else 600;
+          displayOffTimeout = 20;
+        in
+        {
+          enable = true;
+          timeouts = [
+            # lock and suspend
+            {
+              timeout = lockTimeout;
+              command = lock;
+              resumeCommand = display "on";
+            }
+
+            # turn off display after lock (for hosts where suspend disabled)
+            {
+              timeout = lockTimeout + displayOffTimeout;
+              command = display "off";
+              resumeCommand = display "on";
+            }
+
+            # turn off display while idle on lockscreen
+            {
+              timeout = displayOffTimeout;
+              command = "${pkgs.procps}/bin/pgrep gtklock && { ${lock}; ${display "off"}; }; }";
+              resumeCommand = display "on";
+            }
+          ];
+        };
 
       home.file.".config/noctalia" = {
         source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/noctalia";
