a806f56ab75ad6a0bc55e73ad3ea0b920f278ae2
Niakr1s <pavel2188@gmail.com>
2026-07-01 13:02:50 +0300

    configured swayidle

--- modules/02-desktop/window-managers/mango.nix
+++ modules/02-desktop/window-managers/mango.nix
@@ -22,6 +22,7 @@
     satty
     wayfreeze
     wlr-randr
+    wlopm
   ];
 
   security.polkit.enable = true; # polkit
@@ -46,35 +47,38 @@
   home-manager.users.${username} =
     { config, ... }:
     {
-      # services.swayidle =
-      #   let
-      #     lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call sessionMenu lockAndSuspend";
-      #     displayOff = "notify-send displayoff"; # TODO
-      #     lockTimeout = if hostname == "laptop" then 300 else 600;
-      #     displayOffTimeout = 20;
-      #   in
-      #   {
-      #     enable = true;
-      #     timeouts = [
-      #       # lock and suspend
-      #       {
-      #         timeout = lockTimeout;
-      #         command = lock;
-      #       }
-      #
-      #       # turn off display after lock (for hosts where suspend disabled)
-      #       {
-      #         timeout = lockTimeout + displayOffTimeout;
-      #         command = displayOff;
-      #       }
-      #
-      #       # turn off display while idle on lockscreen
-      #       {
-      #         timeout = displayOffTimeout;
-      #         command = "${pkgs.procps}/bin/pgrep gtklock && { ${lock}; ${displayOff}; }";
-      #       }
-      #     ];
-      #   };
+      services.swayidle =
+        let
+          lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call sessionMenu lockAndSuspend";
+          display = status: "${pkgs.wlopm}/bin/wlopm --${status} '*'"; # TODO
+          lockTimeout = if hostname == "laptop" then 300 else 600;
+          displayOffTimeout = 20;
+        in
+        {
+          enable = true;
+          timeouts = [
+            # lock and suspend
+            {
+              timeout = lockTimeout;
+              command = lock;
+              resumeCommand = display "on";
+            }
+
+            # turn off display after lock (for hosts where suspend disabled)
+            {
+              timeout = lockTimeout + displayOffTimeout;
+              command = display "off";
+              resumeCommand = display "on";
+            }
+
+            # turn off display while idle on lockscreen
+            {
+              timeout = displayOffTimeout;
+              command = "${pkgs.procps}/bin/pgrep gtklock && { ${lock}; ${display "off"}; }; }";
+              resumeCommand = display "on";
+            }
+          ];
+        };
 
       home.file.".config/mango/config.conf".source =
         config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/mango/config.conf";
