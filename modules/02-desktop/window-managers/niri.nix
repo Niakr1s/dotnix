0ee31eaa205d80d81dc518d25914e2c7c5dd3f9e
Niakr1s <pavel2188@gmail.com>
2026-07-01 15:40:50 +0300

    moved noctalia into niri config

--- modules/02-desktop/window-managers/niri.nix
+++ modules/02-desktop/window-managers/niri.nix
@@ -23,19 +23,68 @@
     "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ]; # or "kde"
   };
 
+  # example command
+  # gtklock -b \"$(jq -r 'first(.wallpapers[].dark) // .defaultWallpaper' ~/.cache/noctalia/wallpapers.json)\" -f
+
+  security.pam.services.gtklock = { };
+  programs.gtklock = {
+    enable = true;
+    modules = with pkgs; [
+      gtklock-virtkb-module
+    ];
+  };
+
   home-manager.users.${username} =
     { config, ... }:
     {
+      services.swayidle =
+        let
+          lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call sessionMenu lockAndSuspend";
+          displayOff = "${pkgs.niri}/bin/niri msg action power-off-monitors";
+          lockTimeout = if hostname == "laptop" then 300 else 600;
+          displayOffTimeout = 20;
+        in
+        {
+          enable = true;
+          timeouts = [
+            # lock and suspend
+            {
+              timeout = lockTimeout;
+              command = lock;
+            }
+
+            # turn off display after lock (for hosts where suspend disabled)
+            {
+              timeout = lockTimeout + displayOffTimeout;
+              command = displayOff;
+            }
+
+            # turn off display while idle on lockscreen
+            {
+              timeout = displayOffTimeout;
+              command = "${pkgs.procps}/bin/pgrep gtklock && { ${lock}; ${displayOff}; }";
+            }
+          ];
+        };
+
+      # main config file
       home.file.".config/niri/config.kdl".source =
         config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/config.kdl";
 
+      # separate configs for different hosts
+      home.file.".config/niri/${hostname}.kdl" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/${hostname}.kdl";
+      };
+
+      # noctalia colorsheme
       home.file.".config/niri/noctalia.kdl" = {
         source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/noctalia.kdl";
         recursive = true;
       };
 
-      home.file.".config/niri/${hostname}.kdl" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/${hostname}.kdl";
+      # noctalia config
+      home.file.".config/noctalia" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/noctalia";
       };
     };
 }
