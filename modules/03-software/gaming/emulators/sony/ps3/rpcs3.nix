2625ddc4999ed93f62bd65c678a81190ec311384
Niakr1s <pavel2188@gmail.com>
2026-07-05 22:18:30 +0300

    fixed rpcs3 error

--- modules/03-software/gaming/emulators/sony/ps3/rpcs3.nix
+++ modules/03-software/gaming/emulators/sony/ps3/rpcs3.nix
@@ -3,12 +3,16 @@
   username,
   flakeDir,
   ...
-}: {
+}:
+{
   environment.systemPackages = with pkgs; [
-    rpcs3
+    # rpcs3 TODO: uncomment htis after flake update, because it seems to be fixed
+    (rpcs3.overrideAttrs (prev: {
+      cmakeFlags = prev.cmakeFlags ++ [ (lib.cmakeBool "BUILD_SHARED_LIBS" false) ];
+    }))
   ];
 
-  home-manager.users.${username} = {config, ...}: {
+  home-manager.users.${username} = { config, ... }: {
     home.file.".config/rpcs3/config.yml" = {
       source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/rpcs3/config.yml";
       force = true;
