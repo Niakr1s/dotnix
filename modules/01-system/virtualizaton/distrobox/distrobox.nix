ea3b62a3bccaf42da20891fa7681cb78ee475a8b
Niakr1s <pavel2188@gmail.com>
2026-07-09 10:37:14 +0300

    added distrobox.nix

---
+++ modules/01-system/virtualizaton/distrobox/distrobox.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  username,
+  ...
+}:
+{
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    distrobox
+    shadow
+  ];
+
+  environment.etc."distrobox/distrobox.conf".text = ''
+    container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+  '';
+
+  users.users.${username} = {
+    extraGroups = [ "podman" ];
+    subGidRanges = [
+      {
+        count = 65536;
+        startGid = 1000;
+      }
+    ];
+    subUidRanges = [
+      {
+        count = 65536;
+        startUid = 1000;
+      }
+    ];
+  };
+}
