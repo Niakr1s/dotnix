ca79c217ad3539721b0284852cab5c3e24ee4771
Niakr1s <pavel2188@gmail.com>
2026-07-10 11:03:39 +0300

    separated dms.nix

---
+++ modules/02-desktop/window-managers/niri/dms.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  username,
+  hostname,
+  flakeDir,
+  ...
+}:
+{
+  programs.dms-shell = {
+    enable = true;
+
+    systemd = {
+      enable = true; # Systemd service for auto-start
+      restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
+    };
+
+    # Core features
+    enableSystemMonitoring = true; # System monitoring widgets (dgop)
+    enableVPN = true; # VPN management widget
+    enableDynamicTheming = true; # Wallpaper-based theming (matugen)
+    enableAudioWavelength = true; # Audio visualizer (cava)
+    enableCalendarEvents = true; # Calendar integration (khal)
+  };
+
+  programs.dsearch.enable = true;
+
+  home-manager.users.${username} =
+    { config, ... }:
+    {
+      # main config file
+      home.file.".config/niri/dms".source =
+        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/dms";
+
+      home.file.".config/DankMaterialShell/firefox.css".source =
+        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/DankMaterialShell/firefox.css";
+
+      home.file.".config/DankMaterialShell/plugin_settings.json".source =
+        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/DankMaterialShell/plugin_settings.json";
+
+      home.file.".config/DankMaterialShell/settings.json".source =
+        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/DankMaterialShell/settings.json";
+    };
+}
--- modules/02-desktop/window-managers/niri/niri.nix
+++ modules/02-desktop/window-managers/niri/niri.nix
@@ -6,6 +6,10 @@
   ...
 }:
 {
+  imports = [
+    ./dms.nix
+  ];
+
   programs.niri = {
     enable = true;
   };
@@ -34,57 +38,9 @@
     ];
   };
 
-  programs.dms-shell = {
-    enable = true;
-
-    systemd = {
-      enable = true; # Systemd service for auto-start
-      restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
-    };
-
-    # Core features
-    enableSystemMonitoring = true; # System monitoring widgets (dgop)
-    enableVPN = true; # VPN management widget
-    enableDynamicTheming = true; # Wallpaper-based theming (matugen)
-    enableAudioWavelength = true; # Audio visualizer (cava)
-    enableCalendarEvents = true; # Calendar integration (khal)
-  };
-
-  programs.dsearch.enable = true;
-
   home-manager.users.${username} =
     { config, ... }:
     {
-      # services.swayidle =
-      #   let
-      #     lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call sessionMenu lockAndSuspend";
-      #     displayOff = "${pkgs.niri}/bin/niri msg action power-off-monitors";
-      #     lockTimeout = if hostname == "laptop" then 300 else 600;
-      #     displayOffTimeout = 20;
-      #   in
-      #   {
-      #     enable = true;
-      #     timeouts = [
-      #       # lock and suspend
-      #       {
-      #         timeout = lockTimeout;
-      #         command = lock;
-      #       }
-      #
-      #       # turn off display after lock (for hosts where suspend disabled)
-      #       {
-      #         timeout = lockTimeout + displayOffTimeout;
-      #         command = displayOff;
-      #       }
-      #
-      #       # turn off display while idle on lockscreen
-      #       {
-      #         timeout = displayOffTimeout;
-      #         command = "${pkgs.procps}/bin/pgrep gtklock && { ${lock}; ${displayOff}; }";
-      #       }
-      #     ];
-      #   };
-
       # main config file
       home.file.".config/niri/config.kdl".source =
         config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/config.kdl";
@@ -93,18 +49,5 @@
       home.file.".config/niri/${hostname}.kdl" = {
         source = config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/${hostname}.kdl";
       };
-
-      # main config file
-      home.file.".config/niri/dms".source =
-        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/niri/dms";
-
-      home.file.".config/DankMaterialShell/firefox.css".source =
-        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/DankMaterialShell/firefox.css";
-
-      home.file.".config/DankMaterialShell/plugin_settings.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/DankMaterialShell/plugin_settings.json";
-
-      home.file.".config/DankMaterialShell/settings.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${flakeDir}/home/.config/DankMaterialShell/settings.json";
     };
 }
