ec71d2bedebec51b3b4a0c49076fff268ce29dfb
Niakr1s <pavel2188@gmail.com>
2026-08-12 20:44:24 +0300

    turned off sleep and hibernation

--- Hosts/desktop/default.nix
+++ Hosts/desktop/default.nix
@@ -16,6 +16,11 @@ in
     (pkgs.writeScriptBin "manage-disks" (builtins.readFile ./scripts/manage-disks.sh))
   ];
 
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   core = {
     user = "user";
 
