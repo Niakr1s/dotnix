8b883b551a5c7bed9b5f66d55c5010630019688a
Niakr1s <pavel2188@gmail.com>
2026-08-12 22:19:57 +0300

    fixed mediainfo plugin

--- Features/Core/yazi.nix
+++ Features/Core/yazi.nix
@@ -5,10 +5,19 @@
   hjem,
   ...
 }:
+let
+  user = config.core.user;
+in
 {
   environment.systemPackages = with pkgs; [
     mediainfo
   ];
+
+  hjem.users.${user} = {
+    xdg.config.files = {
+      "yazi/plugins/mediainfo.yazi".source = pkgs.yaziPlugins.mediainfo;
+    };
+  };
 
   programs.yazi = {
     enable = true;
