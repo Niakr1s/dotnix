b157edf4b2e1196e5175d9eff5a905ccecb9cb41
Niakr1s <pavel2188@gmail.com>
2026-08-12 22:14:38 +0300

    yazi config to chezmoi

--- Features/Core/yazi.nix
+++ Features/Core/yazi.nix
@@ -5,26 +5,13 @@
   hjem,
   ...
 }:
-let
-  user = config.core.user;
-in
 {
   environment.systemPackages = with pkgs; [
     mediainfo
   ];
 
-  hjem.users.${user} = {
-    enable = true;
-    xdg.config.files = {
-      "yazi/yazi.toml".source = ./yazi/yazi.toml;
-      "yazi/keymap.toml".source = ./yazi/keymap.toml;
-      "yazi/plugins/mediainfo.yazi".source = pkgs.yaziPlugins.mediainfo;
-    };
-  };
-
   programs.yazi = {
     enable = true;
-
     package = pkgs.yazi.override { _7zz = pkgs._7zz-rar; };
   };
 }
