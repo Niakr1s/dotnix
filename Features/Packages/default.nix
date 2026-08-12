82751a11d0ce004516ccc6117981bf5a9a99ce86
Niakr1s <pavel2188@gmail.com>
2026-08-12 23:26:56 +0300

    simplified a lot

--- Features/Packages/default.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }: {
-  imports = lib.mapAttrsToList (name: _: ./${name}) (
-    lib.filterAttrs (
-      name: type:
-      (type == "directory") || (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
-    ) (builtins.readDir ./.)
-  );
-}
--- Features/default.nix
+++ Features/default.nix
@@ -1,5 +1,25 @@
-{ lib, ... }: {
-  imports = lib.mapAttrsToList (name: _: ./${name}) (
-    lib.filterAttrs (name: type: type == "directory") (builtins.readDir ./.)
-  );
+{ lib, ... }:
+let
+  # 1. Получаем список всех элементов в корневой директории
+  rootDirContents = builtins.readDir ./.;
+
+  # 2. Фильтруем, оставляя только папки первого уровня
+  subDirs = lib.filterAttrs (name: type: type == "directory") rootDirContents;
+
+  # 3. Функция, которая читает содержимое ОДНОЙ подпапки и собирает её .nix файлы
+  findNixInSubdir = dirName:
+    let
+      subdirPath = ./${dirName};
+      subdirContents = builtins.readDir subdirPath;
+    in
+    lib.mapAttrsToList (name: _: subdirPath + "/${name}") (
+      lib.filterAttrs (name: type:
+        type == "directory" || type == "regular" && lib.hasSuffix ".nix" name
+      ) subdirContents
+    );
+in
+{
+  # 4. Собираем файлы из всех подпапок в один плоский список
+  imports = lib.flatten (lib.mapAttrsToList (name: _: findNixInSubdir name) subDirs);
 }
+
