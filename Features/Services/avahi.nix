78b67cee59757d14a6d72a6575266cbca27ab60c
Niakr1s <pavel2188@gmail.com>
2026-08-13 00:25:37 +0300

    added avahi

---
+++ Features/Services/avahi.nix
@@ -0,0 +1,19 @@
+{
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true; # Integrates .local resolution with your system's DNS
+    openFirewall = true;
+    wideArea = false; # Enabling `services.avahi.wideArea` exposes this system to `CVE-2024-52615`
+    denyInterfaces = [
+      "virbr0"
+      "docker0"
+    ];
+    publish = {
+      enable = true; # Allows services to be published
+      addresses = true; # Publishes the machine's IP addresses
+      workstation = true;
+      userServices = true;
+    };
+  };
+}
+
