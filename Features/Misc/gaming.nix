2f4a551f1c273a376726c6e72497bfdf0882cb81
Niakr1s <pavel2188@gmail.com>
2026-08-13 01:23:36 +0300

    added link_steamruntime_to_umu script

--- Features/Misc/gaming.nix
+++ Features/Misc/gaming.nix
@@ -38,6 +38,18 @@ in
     environment.systemPackages = with pkgs; [
       mangohud
       lutris
+      (pkgs.writeScriptBin "link_steamruntime_to_umu" ''
+          rm -rf ~/.local/share/umu/steamrt*
+          ln -s ~/.local/share/Steam/steamapps/common/SteamLinuxRuntime ~/.local/share/umu/steamrt1
+          ln -s ~/.local/share/Steam/steamapps/common/SteamLinuxRuntime ~/.local/share/umu/steamrt # dunno if it with prefix 1
+          ln -s ~/.local/share/Steam/steamapps/common/SteamLinuxRuntime_soldier ~/.local/share/umu/steamrt2
+          ln -s ~/.local/share/Steam/steamapps/common/SteamLinuxRuntime_sniper ~/.local/share/umu/steamrt3
+          ln -s ~/.local/share/Steam/steamapps/common/SteamLinuxRuntime_4 ~/.local/share/umu/steamrt4
+      '')
+    ];
+
+    warnings = [
+      "You can download all steam runtimes from steam tools and link them via 'link_steamruntime_to_umu' script"
     ];
 
     programs = {
