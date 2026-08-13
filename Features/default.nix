4f7361e8773c9c2ca4b8f820205a6452bd50c61f
Niakr1s <pavel2188@gmail.com>
2026-08-13 19:54:17 +0300

    made flakeLib.importSubdirs

--- Features/default.nix
+++ Features/default.nix
@@ -1,25 +1,4 @@
-{ lib, ... }:
-let
-  # 1. Получаем список всех элементов в корневой директории
-  rootDirContents = builtins.readDir ./.;
-
-  # 2. Фильтруем, оставляя только папки первого уровня
-  subDirs = lib.filterAttrs (name: type: type == "directory") rootDirContents;
-
-  # 3. Функция, которая читает содержимое ОДНОЙ подпапки и собирает её .nix файлы
-  findNixInSubdir = dirName:
-    let
-      subdirPath = ./${dirName};
-      subdirContents = builtins.readDir subdirPath;
-    in
-    lib.mapAttrsToList (name: _: subdirPath + "/${name}") (
-      lib.filterAttrs (name: type:
-        type == "directory" || type == "regular" && lib.hasSuffix ".nix" name
-      ) subdirContents
-    );
-in
 {
-  # 4. Собираем файлы из всех подпапок в один плоский список
-  imports = lib.flatten (lib.mapAttrsToList (name: _: findNixInSubdir name) subDirs);
-}
-
+  flakeLib,
+  ...
+} : flakeLib.importSubdirs ./.
--- flakeLib.nix
+++ flakeLib.nix
@@ -13,7 +13,7 @@
         enable = true;
         virtualHosts."${name}.localhost" = {
           extraConfig = ''
-            reverse_proxy localhost:${toString port} 
+            reverse_proxy localhost:${toString port}
             ${
               if insecureTLS then
                 ''
@@ -57,4 +57,36 @@
     {
       systemd.tmpfiles.rules = map (dir: "d ${dir} ${mode} ${user} ${group} - -") dirs;
     };
+
+  importSubdirs =
+    path:
+    let
+      # 1. Получаем список всех элементов в корневой директории
+      rootDirContents = builtins.readDir path;
+
+      # 2. Фильтруем, оставляя только папки первого уровня
+      subDirs = lib.filterAttrs (name: type: type == "directory") rootDirContents;
+
+      # 3. Функция, которая читает содержимое ОДНОЙ подпапки и собирает её .nix файлы
+      findNixInSubdir =
+        dirName:
+        let
+          subdirPath = path + "/${dirName}";
+          subdirContents = builtins.readDir subdirPath;
+        in
+        lib.mapAttrsToList (name: _: subdirPath + "/${name}") (
+          lib.filterAttrs (
+            name: type:
+            (type == "regular" && lib.hasSuffix ".nix" name)
+            ||
+              # Safer check: only import subdirs if they actually have a default.nix
+              (type == "directory" && builtins.pathExists (subdirPath + "/${name}/default.nix"))
+          ) subdirContents
+        );
+    in
+    {
+      # 4. Собираем файлы из всех подпапок в один плоский список
+      imports = lib.flatten (lib.mapAttrsToList (name: _: findNixInSubdir name) subDirs);
+    };
+
 }
