bbd25afc359719cba8bd4529dd60fd341ff4d3c7
Niakr1s <pavel2188@gmail.com>
2026-08-15 19:39:42 +0300

    added warning

--- modules/packages/gui/winboat.nix
+++ modules/packages/gui/winboat.nix
@@ -9,6 +9,12 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
+    warnings = [
+      ''
+        after installing windows in winboat, you can add a shared directory in "/home/user/.winboat/podman-compose.yml" under "volumes":  /data:/shared2
+                after that you need to do this manually: "docker compose down && docker compose up -d"''
+    ];
+
     nixpkgs.config.permittedInsecurePackages = [
       "electron-40.10.5"
     ];
