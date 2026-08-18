fe1e4417d7cb19ef6930715cd6f42238b5db7782
Niakr1s <pavel2188@gmail.com>
2026-08-18 14:09:06 +0300

    added laptop host

---
+++ hosts/laptop/default.nix
@@ -0,0 +1,78 @@
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./hardware.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    lisgd
+  ];
+
+  modules = {
+    core = {
+      user = "user";
+      headless = false;
+
+      gpu = {
+        intel.enable = true;
+      };
+
+      cpu.intel = true;
+
+      zram = {
+        enable = true;
+        percent = 200;
+      };
+    };
+
+    de = {
+      niri.enable = true;
+    };
+
+    packages = {
+      cli = {
+        nvim.enable = true;
+
+        dev = {
+          buildtools = true;
+          langs = {
+            bundles.functional = false;
+
+            cpp = true;
+            go = true;
+            haskell = false;
+            java = true;
+            lua = true;
+            node = true;
+            perl = false;
+            php = false;
+            python = true;
+            ruby = false;
+            rust = true;
+            zig = true;
+          };
+        };
+      };
+      gui = {
+        enable = true;
+        zed.enable = true;
+        winboat.enable = false;
+        blender.enable = false;
+        obs.enable = false;
+      };
+      lsp = {
+        base = true;
+        heavy = false;
+      };
+    };
+
+    services = {
+      avahi.enable = true;
+      syncthing.enable = true;
+      v2raya.enable = true;
+    };
+  };
+}
