cf04e35ea15de8b9df7943e6491e974568c33b07
Niakr1s <pavel2188@gmail.com>
2026-08-18 15:13:56 +0300

    moved kernelParams to bootloader

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -26,12 +26,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  boot.kernelParams = [
-    "clearcpuid=514" # probable fix for errors in proton (umip: For now, expensive software emulation returns the result)
-    "split_lock_detect=off" # probable fix for errors in proton(x86/split lock detection: took a split_lock trap at address)
-    "usbcore.quirks=\"057e:2009:ik\""
-  ];
-
   fileSystems."/" = {
     device = "/dev/mapper/luks-276c86e7-7ad1-4624-ae05-37a41127565f";
     fsType = "btrfs";
--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -20,11 +20,15 @@ in
       enable = !headless;
       theme = "bgrt";
     };
-    kernelParams = mkIf (!headless) [
+    kernelParams = [
+      "clearcpuid=514" # probable fix for errors in proton (umip: For now, expensive software emulation returns the result)
+      "split_lock_detect=off" # probable fix for errors in proton(x86/split lock detection: took a split_lock trap at address)
+      "usbcore.quirks=\"057e:2009:ik\"" # fix for gamepad
+    ] ++ (lib.optionals (!headless) [
       "quiet"
       "splash"
       "udev.log_level=3"
-    ];
+    ]);
     initrd.systemd.enable = true;
   };
 }
