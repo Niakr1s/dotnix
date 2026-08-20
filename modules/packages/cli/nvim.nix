f8801be7f9e0fa032736addd624447ac4efd95f3
Niakr1s <pavel2188@gmail.com>
2026-08-20 22:56:39 +0300

    added some more formatters

--- modules/packages/cli/nvim.nix
+++ modules/packages/cli/nvim.nix
@@ -34,25 +34,35 @@
   formatters = with pkgs; [
     # --- baseServers Languages ---
     alejandra # Nix (nixd)
-    stylua # Lua (lua_ls)
-    shfmt # Bash / Shell (bashls)
-    ruff # Python Lint/Format (ruff/pyright)
-    biome # JavaScript / TypeScript / JSON / HTML / CSS (ts_ls, jsonls, html, cssls)
-    yamlfmt # YAML (yamlls)
-    deno # Markdown (marksman) - Multi-language block fallback
-    dockerfile-language-server # Dockerfile (dockerls) - (Often formats natively or via 'injected')
-    taplo # TOML (taplo)
-    lemminx # XML (lemminx) - Formats natively via its own LSP binaries
-    sleek # SQL (sqlls) - Modern CLI SQL formatter
-
-    # --- heavyServers Languages ---
-    clang-tools # C / C++ (clangd) - Provides 'clang-format'
-    ktlint # Kotlin (kotlin_language_server)
-    rustfmt # Rust (rust_analyzer) - Usually bundled, explicit here
-    gofumpt # Go (gopls) - Stricter standard formatter
-    csharpier # C# (omnisharp)
-    google-java-format # Java (jdtls)
-    crystal # Crystal (crystalline) - Includes the native
+    beautysh
+    black
+    blade-formatter
+    buf
+    clang-tools
+    csharpier
+    dockerfmt
+    erlfmt
+    fixjson
+    gofumpt
+    goimports-reviser
+    google-java-format
+    hclfmt
+    isort
+    just
+    kdlfmt
+    mago
+    markdown-toc
+    mixtool
+    prettier
+    prettierd
+    ruff
+    rustfmt
+    sqlfluff
+    stylua
+    swiftformat
+    taplo
+    xmlformat
+    yamlfmt
   ];
 
   baseServers = [
@@ -103,9 +113,9 @@ in {
       VISUAL = "nvim";
     };
 
-    environment.systemPackages =
-      [
+    environment.systemPackages = with pkgs; [
         myNvim
+        tree-sitter
       ]
       ++ formatters;
 
